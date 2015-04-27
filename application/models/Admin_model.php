b6bf0f84e4a5b55eb67d63e4804ad55b9c45d2ff
hedii <hedi.chaibs@gmail.com>
2015-04-27 16:43:12 +0200

    add admin model

---
+++ application/models/Admin_model.php
@@ -0,0 +1,24 @@
+<?php
+defined('BASEPATH') OR exit('No direct script access allowed');
+
+/**
+ * Admin_model class.
+ * 
+ * @extends CI_Model
+ */
+class Admin_model extends CI_Model {
+
+	/**
+	 * __construct function.
+	 * 
+	 * @access public
+	 * @return void
+	 */
+	public function __construct() {
+		
+		parent::__construct();
+		$this->load->database();
+		
+	}
+	
+}

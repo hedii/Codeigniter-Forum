e2227cf42c8686f6bd02dd5f8faf5a327221d9c0
hedii <hedi.chaibs@gmail.com>
2015-04-28 16:57:29 +0200

    installation test 3

--- application/config/config.php
+++ application/config/config.php
@@ -17,7 +17,7 @@ defined('BASEPATH') OR exit('No direct script access allowed');
 | environments.
 |
 */
-$config['base_url'] = 'http://codeigniter-forum.dev/';
+$config['base_url'] = '';
 
 /*
 |--------------------------------------------------------------------------
